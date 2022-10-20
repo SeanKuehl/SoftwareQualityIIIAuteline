df4ae998bdeae6c4047c0be6c75f201190d8fba5
SeanKuehl <teddscottfelt@gmail.com>
2022-10-20 12:39:48 -0400

    another attempt to fix CI test problems

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+
+package test.java;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+}
