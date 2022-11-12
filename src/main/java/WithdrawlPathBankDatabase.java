b608a1b17d5e2d21105f943005584808027452db
SeanKuehl <teddscottfelt@gmail.com>
2022-11-12 12:51:45 -0500

    added UI mockups link to readme file

--- src/main/java/WithdrawlPathBankDatabase.java
+++ src/main/java/WithdrawlPathBankDatabase.java
@@ -3,13 +3,13 @@ package main.java;
 public class WithdrawlPathBankDatabase extends BankDatabase{
 
 
-    //   public int debitTest(int userAccountNumber, double amount) {
-    //    double amountInBank = getAccount(userAccountNumber).getAvailableBalance();
-    //    if (amount > amountInBank){
-    //        return 0;
-    //    }
-    //    else {
-    //        return 1;
-    //   }
-    // }
+       public int debitTest(int userAccountNumber, double amount) {
+            double amountInBank = getAccount(userAccountNumber).getAvailableBalance();
+            if (amount > amountInBank){
+                return 0;
+            }
+            else {
+                return 1;
+           }
+     }
 }
